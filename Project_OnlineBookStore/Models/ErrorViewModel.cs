5bf9b554b84232e132677c2eb417da84f5a0f02b
Arati121 <avbhosale@mitaoe.ac.in>
2022-04-25 13:20:47 +0530

    book

---
+++ Project_OnlineBookStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Project_OnlineBookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

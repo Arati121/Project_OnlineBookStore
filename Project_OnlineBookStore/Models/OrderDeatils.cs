3da0e67f63d4cc27cea6556ab4c23717bf39ffd4
Arati121 <avbhosale@mitaoe.ac.in>
2022-04-26 16:08:03 +0530

    order

---
+++ Project_OnlineBookStore/Models/OrderDeatils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_OnlineBookStore.Models
+{
+    public class OrderDeatils
+    {
+        public int OId { get; set; }
+        public int Quantity { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
---
+++ Project_OnlineBookStore/Models/Orders.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_OnlineBookStore.Models
+{
+    public class Orders
+    {
+        [Key]
+        public int OId { get; set; }
+        public int BId { get; set; }
+        public int UId { get; set; }
+    }
+}
